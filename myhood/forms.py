70396b9ddd5d276e171deb328d199e2607304c3a
celine <chepceline25@gmail.com>
2022-06-17 15:01:01 +0300

    add newpost form

--- myhood/forms.py
+++ myhood/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import fields
 from django.forms import ModelForm
-from .models import Neighborhood
+from .models import Post, Business, Profile
 
 class CreateUserForm(UserCreationForm):
     class Meta:
@@ -13,3 +13,22 @@ class CreateUserForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         self.fields['username'].widget.attrs.pop("autofocus", None)
 
+class NewPostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'post', 'type']
+
+class NewBusinessForm(ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['user', 'neighborhood']
+
+class EditProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+class ChangeNeighborhoodForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['neighborhood']
