c76fb3103b29f45c5ebc525ef64187bf251ede4e
celine <chepceline25@gmail.com>
2022-06-17 13:46:54 +0300

    register models

--- myhood/admin.py
+++ myhood/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Neighborhood,Gender, Profile,Post, PostType
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Gender)
+admin.site.register(Post)
+admin.site.register(Neighborhood)
+#admin.site.register(Business)
+admin.site.register(PostType)
