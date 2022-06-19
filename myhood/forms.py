ff027e0a629d008ec93bfacca4daaaec4f661178
celine <chepceline25@gmail.com>
2022-06-19 09:29:52 +0300

    run tests

--- myhood/forms.py
+++ myhood/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import fields
 from django.forms import ModelForm
-from .models import Post, Business, Profile
+from .models import Neighborhood, Post, Business, Profile
 
 class CreateUserForm(UserCreationForm):
     class Meta:
@@ -17,6 +17,13 @@ class NewPostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'post', 'type']
+
+class AddNeighborhoodForm(ModelForm):
+    class Meta:
+        model = Neighborhood
+        fields = ['name', 'location', 'description', 'manager_name', 'manager_number', 'manager_email', 'hospital_name', 'hospital_number', 'hospital_email', 'police_name', 'police_number', 'police_email', 'hood_pic']       
+
+
 
 class NewBusinessForm(ModelForm):
     class Meta:
--- myhood/tests.py
+++ myhood/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from .models import Neighborhood, Gender, Profile, PostType, Post, Business
 
 # Create your tests here.
+
+class NeighborhoodTestClass(TestCase):
+    def setUp(self):
+        self.name=Neighborhood(name='kalimoni', location='juja', description='good neighborhood', manager_name='Harley', manager_number='0701234567', manager_email='harley@gmail.com', hospital_name='kalimoni', hospital_number='0701234567', hospital_email='kdhs@gmail.com', police_name='kalimoni police station', police_number='0701234567', police_email='kps@gmail.com', hood_pic='image')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.name,Neighborhood))  
+
+    def test_create_method(self):
+        self.name.create_neighborhood()
+        neighborhood=Neighborhood.objects.all()
+        self.assertTrue(len(neighborhood) > 0) 
+
+    def test_delete_method(self):
+        self.name.create_neighborhood()
+        self.name.delete()
+        neighborhoods=Neighborhood.objects.all()
+        self.assertTrue(len(neighborhoods) == 0) 
+
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.user=Profile(user='Faith', house='254', phase='3', phone='0701234567', gender='femele/male', profile_pic='image', neighborhood='kalimoni', )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user,Profile))    
+
+
+
+
